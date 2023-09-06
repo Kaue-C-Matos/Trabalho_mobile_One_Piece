f125e797dae1cb88ff8b6653d5ae91a8b992852d
Kaue-C-Matos <kauecostamatos31@gmail.com>
2023-09-06 14:35:33 -0300

    Hoje finalizei os detalhes da tela de Login, adic ionei uma navegação para ir para a página de listas ao pressionar "entrar" e agora possui uma validação quanto ao nome e senha nome: admin senha: senha123

--- App.js
+++ App.js
@@ -2,14 +2,22 @@ import { GluestackUIProvider, Text, Box, config, Center } from '@gluestack-ui/th
 import { SafeAreaView, StatusBar } from 'react-native';
 import Login from './src/screens/Login';
 import CharacterList from './src/screens/CharacterList';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
   return (
-    <SafeAreaView>
-      <StatusBar/>
-      <GluestackUIProvider config={config.theme}>
-          <Login/>
-      </GluestackUIProvider>
-    </SafeAreaView>
+    <GluestackUIProvider config={config.theme}>
+      
+        <NavigationContainer>      
+          <Stack.Navigator>
+            <Stack.Screen name='Login' component={Login}/>
+            <Stack.Screen name='Characters' component={CharacterList}/>
+          </Stack.Navigator>
+         </NavigationContainer>
+              
+    </GluestackUIProvider>
   );
 }
--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,11 +1,31 @@
-import { Center, Text, Input, InputField, Image, Button, ButtonText } from "@gluestack-ui/themed";
+import { Center, Text, Input, InputField, Image, Button, ButtonText, Alert, AlertIcon, AlertText, InfoIcon, Box, InputIcon, EyeIcon, EyeOffIcon, Icon, Switch, } from "@gluestack-ui/themed";
+import { useState } from "react";
 import { ImageBackground } from "react-native";
 
 const bg = {uri: "https://i.pinimg.com/originals/61/89/21/61892182d2c104029a5c1a201f2b4afe.jpg"}
 const logo = {uri: "https://www.pngmart.com/files/13/One-Piece-Logo-PNG-Image.png"}
 
-const Login = () => {
+const Login = ({ navigation }) => {
+    const [user, setUser] = useState()
+    const [password, setPassword] = useState()
+    const [validUser, setValidUser] = useState()
+    const [switchValue, setSwitchValue] = useState(false)
+
+    const validateUser = () =>{
+        if (user == "admin" && password == "senha123"){
+            setValidUser(true)
+        } else {
+            setValidUser(false)
+        }
+    }
+
+    const changeSwitchValue = () =>{
+        setSwitchValue(!switchValue)
+    }
+
     return (
+        
+
         <ImageBackground source={bg} resizeMode="cover">
             <Center h="$full">
                 <Image 
@@ -16,16 +36,30 @@ const Login = () => {
                 />
 
                 <Text color="#fff" bold>Nome:</Text>
-                <Input variant="rounded" size="md" width="$2/3">
-                    <InputField bg="#fff"/>
+                <Input variant="rounded" size="md" width="$2/3" >
+                    <InputField bg="#fff" onChangeText={setUser}/>
                 </Input>
                 
+                
                 <Text color="#fff"bold marginTop="$5">Senha:</Text>
-                <Input variant="rounded" size="md" width="$2/3">
-                    <InputField type="password" bg="#fff"/>
+                <Input variant="rounded" size="md" width="$2/3" >
+                    <InputField type={switchValue ? "text" : "password"} bg="#fff" onChangeText={setPassword}/>
+                    <Switch value={switchValue} onValueChange={changeSwitchValue} bgColor="#fff"/>
                 </Input>
 
-                <Button size="md" variant="solid" marginTop="$5">
+                {!validUser ? <Box bg='$yellow100' marginHorizontal="$12" marginVertical="$5">
+                    <Text color="red" bold italic  hardShadow="$5" padding="$1">Usuário ou senha incorretos, por favor revise suas credenciais</Text>
+                    </Box>
+                 : <></>}
+                
+
+                <Button size="md" variant="solid" marginTop="$5" 
+                    onPress={() =>{validateUser(); 
+                        if(validUser){
+                            navigation.navigate("Characters")
+                        }
+                    }}
+                >
                 <ButtonText>Entrar </ButtonText>
                 </Button>
       
